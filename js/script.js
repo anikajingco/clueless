c1f818063d66154107c2067c68d5b8f2ff534387
Anika Jingco <62725158+anikajingco@users.noreply.github.com>
2021-09-10 13:28:50 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// smooth animation between jump links
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+// character popups
+$(".polaroid").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
